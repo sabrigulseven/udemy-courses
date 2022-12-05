80659b091a69245aff0e7846452998ea7d575a47
Sabri Gülseven <sabrigulseven@outlook.com>
2022-12-05 14:34:23 +0300

    Response Header - Cookie

--- java-IoC-CDI-SpringBoot/Spring-boot/springrest/src/main/java/com/godoro/springrest/sports/client/TransferController.java
+++ java-IoC-CDI-SpringBoot/Spring-boot/springrest/src/main/java/com/godoro/springrest/sports/client/TransferController.java
@@ -36,4 +36,26 @@ public class TransferController {
 
 		return "Çerez gönderildi: " + responseEntity.getBody();
 	}
+
+	@GetMapping("/client/setheader")
+	@ResponseBody
+	public String setHeader() {
+		String url = "http://localhost:8080/transfer/setheader";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY,
+				String.class);
+		String inputString = responseEntity.getHeaders().getFirst("input");
+		return "Başlık gönderildi: " + responseEntity.getBody() + " başlık: " + inputString;
+	}
+
+	@GetMapping("/client/setcookie")
+	@ResponseBody
+	public String setCookie() {
+		String url = "http://localhost:8080/transfer/setcookie";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY,
+				String.class);
+		String inputString = responseEntity.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
+		return "Çerez gönderildi: " + responseEntity.getBody() + " başlık: " + inputString;
+	}
 }
--- java-IoC-CDI-SpringBoot/Spring-boot/springrest/src/main/java/com/godoro/springrest/sports/service/TransferResource.java
+++ java-IoC-CDI-SpringBoot/Spring-boot/springrest/src/main/java/com/godoro/springrest/sports/service/TransferResource.java
@@ -1,6 +1,6 @@
 package com.godoro.springrest.sports.service;
 
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TransferResource {
 	@GetMapping("/transfer/header")
-	public ResponseEntity<?> getHeader(@RequestHeader(value = "input",defaultValue = "varsayılan") String input) {
+	public ResponseEntity<?> getHeader(@RequestHeader(value = "input", defaultValue = "varsayılan") String input) {
 		String output = "Giren: " + input;
 		return ResponseEntity.ok(output);
-				
+
 	}
-	
+
 	@GetMapping("/transfer/cookie")
 	public ResponseEntity<?> getCookie(@CookieValue(value = "input") String input) {
 		String output = "Giren: " + input;
 		return ResponseEntity.ok(output);
-				
+
+	}
+
+	@GetMapping("/transfer/setheader")
+	public ResponseEntity<?> setHeader() {
+		//HttpHeaders headers = new HttpHeaders();
+		//headers.add("input", "Godoro");
+		return ResponseEntity.ok()
+				//.headers(headers)
+				.header("input", "Godoro")
+				.body("Başlık Gönderildi");
+
+	}
+	
+	@GetMapping("/transfer/setcookie")
+	public ResponseEntity<?> setCookie() {
+		return ResponseEntity.ok()
+				.header(HttpHeaders.SET_COOKIE, "Godoro")
+				.body("Cookie Gönderildi");
+
 	}
 }
