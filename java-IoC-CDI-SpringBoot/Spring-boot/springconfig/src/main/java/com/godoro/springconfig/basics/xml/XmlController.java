5cf4bac5b4352ca9e9a1df73a26708e998f78763
Sabri Gülseven <sabrigulseven@outlook.com>
2022-12-10 14:08:30 +0300

    XmlBeans Advanced

--- java-IoC-CDI-SpringBoot/Spring-boot/springconfig/src/main/java/com/godoro/springconfig/basics/xml/XmlController.java
+++ java-IoC-CDI-SpringBoot/Spring-boot/springconfig/src/main/java/com/godoro/springconfig/basics/xml/XmlController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.godoro.springconfig.basics.bean.MyAgent;
 import com.godoro.springconfig.basics.bean.MyBean;
+import com.godoro.springconfig.basics.bean.MySpec;
 
 @Controller
 public class XmlController {
@@ -17,5 +19,23 @@ public class XmlController {
         MyBean myBean = (MyBean) context.getBean("myBean");
         context.close();
         return "Çekirdek " + myBean;
+    }
+
+    @GetMapping("/xml/reference")
+    @ResponseBody
+    public String getReference() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("springBeans.xml");
+        MyAgent myAgent = (MyAgent) context.getBean("myAgent");
+        context.close();
+        return "Kapsam " + myAgent.getAgentName() + "  " + myAgent.getMyBean().toString();
+    }
+
+    @GetMapping("/xml/inversion")
+    @ResponseBody
+    public String getInversion() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("springBeans.xml");
+        MySpec mySpec = (MySpec) context.getBean("mySpec");
+        context.close();
+        return "Evirtme >" + mySpec.myMethod("Girdi");
     }
 }
