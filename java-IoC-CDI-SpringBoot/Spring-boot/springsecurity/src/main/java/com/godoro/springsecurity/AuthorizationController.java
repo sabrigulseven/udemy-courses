735c158b353e6ea8b048337e1e62da57826ef1f1
Sabri Gülseven <sabrigulseven@outlook.com>
2022-12-15 14:25:03 +0300

    Authorization & Roles

---
+++ java-IoC-CDI-SpringBoot/Spring-boot/springsecurity/src/main/java/com/godoro/springsecurity/AuthorizationController.java
@@ -0,0 +1,36 @@
+package com.godoro.springsecurity;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AuthorizationController {
+	
+	@GetMapping("/high")
+	public String getHigh() {
+		return "high/HighPage";
+	}
+	
+	@GetMapping("/low")
+	public String getLow() {
+		return "low/LowPage";
+	}
+	
+	@GetMapping("/currentroles")
+	@ResponseBody
+	public String getCurrent() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		StringBuilder builder = new StringBuilder(); 
+		if (authentication!=null) {
+			for(GrantedAuthority authority : authentication.getAuthorities()) {
+				builder.append(authority.getAuthority()).append(" ");
+			}
+			return "Yetkililer: " + builder.toString();
+		}
+		return "giriş yapılmamış";
+	}
+}
--- java-IoC-CDI-SpringBoot/Spring-boot/springsecurity/src/main/java/com/godoro/springsecurity/authorization/SecurityAuthorization.java
+++ java-IoC-CDI-SpringBoot/Spring-boot/springsecurity/src/main/java/com/godoro/springsecurity/authorization/SecurityAuthorization.java
@@ -1,51 +1,45 @@
-package com.godoro.springsecurity.database;
+package com.godoro.springsecurity.authorization;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class SecurityDatabase{
-
+@Configuration
+public class SecurityAuthorization {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http
 			.authorizeRequests()
 			.antMatchers("/","/home").permitAll()
-			.antMatchers("/login").permitAll()
+			.antMatchers("/low").hasRole("USER")
+			.antMatchers("/high").hasRole("ADMIN")
 			.anyRequest()
 			.authenticated()
 			.and()
 		.formLogin()
-			.loginPage("/login")
-			.defaultSuccessUrl("/welcome")
 			.and()
-		.logout()
-			.logoutSuccessUrl("/logout")
-			.invalidateHttpSession(true)
-			.permitAll();
+		.httpBasic();
 
 		return http.build();
 	}
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.userDetailsService(userDetailsService())
-			.passwordEncoder(passwordEncoder());
+			.inMemoryAuthentication()
+			.withUser("godoro")
+				.password("{noop}java")
+				.roles("USER","ADMIN")
+				.and()
+			.withUser("FİBİLER")
+				.password("{noop}spring")
+				.roles("USER")
+				.and();
+			
 	}	
-	@Bean
-	public static UserDetailsService userDetailsService() {
-		return new UserDetailServiceImpl();
-	}
-	@Bean
-	public static PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 }
