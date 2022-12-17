0ba9e97ea7526982db58d78e06002457e97ca2cf
Sabri Gülseven <sabrigulseven@outlook.com>
2022-12-17 17:01:19 +0300

    Locations & Static Files

--- java-IoC-CDI-SpringBoot/Spring-boot/springtemplates/src/main/java/com/godoro/springtemplates/TemplateController.java
+++ java-IoC-CDI-SpringBoot/Spring-boot/springtemplates/src/main/java/com/godoro/springtemplates/TemplateController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TemplateController {
+	
 	@GetMapping(value = "/template/index")
 	public String getIndex(Model model) {
 		model.addAttribute("message","Bu bir iletidir");
 		return "index";
 	}
-
+	
+	@GetMapping(value = "/template/location")	
+	public String getLocation(Model model) {
+		model.addAttribute("message","Bu bir iletidir");
+		return "myfolder/LocationPage";
+	}
 }
