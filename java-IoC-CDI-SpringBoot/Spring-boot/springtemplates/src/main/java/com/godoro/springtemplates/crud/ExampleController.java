4a62f28f9d224b005b2682f23f022a5fe0bfe35f
Sabri Gülseven <sabrigulseven@outlook.com>
2022-12-19 14:04:09 +0300

    Error Messages

--- java-IoC-CDI-SpringBoot/Spring-boot/springtemplates/src/main/java/com/godoro/springtemplates/crud/ExampleController.java
+++ java-IoC-CDI-SpringBoot/Spring-boot/springtemplates/src/main/java/com/godoro/springtemplates/crud/ExampleController.java
@@ -3,6 +3,7 @@ package com.godoro.springtemplates.crud;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,14 +19,22 @@ public class ExampleController {
 		} else {
 			example = new Example(exampleId, "Örnek Ad1", 12.34);
 		}
+		model.addAttribute("message", "Biçimi Doldurun");
 		model.addAttribute("example", example);
 		return "/crud/ExampleEdit";
 	}
 
 	@PostMapping(path = { "/example/edit", "/example/edit/{id}" })
 	public String editProduct(Model model, Example example, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			result.addError(new ObjectError("example", "Biçimde yanlışlık var"));
+			return "/crud/ExampleEdit";
+		}
+		
 		System.out.println("Saklanıyor " + example.getExampleId() + " " + example.getExampleName() + " "
 				+ example.getExampleValue());
+		model.addAttribute("message", "Başarıyla Saklandı");
 		model.addAttribute("example", example);
 		return "/crud/ExampleEdit";
 
