744b25a7d060612bc2c966b13faaf76e42b259be
Sabri Gülseven <sabrigulseven@outlook.com>
2022-12-19 14:37:53 +0300

    Listing & Delete

--- java-IoC-CDI-SpringBoot/Spring-boot/springtemplates/src/main/java/com/godoro/springtemplates/crud/ExampleController.java
+++ java-IoC-CDI-SpringBoot/Spring-boot/springtemplates/src/main/java/com/godoro/springtemplates/crud/ExampleController.java
@@ -1,5 +1,8 @@
 package com.godoro.springtemplates.crud;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ExampleController {
 
 	@GetMapping(path = { "/example/edit", "/example/edit/{id}" })
-	public String editProduct(Model model, @PathVariable(name = "id", required = false) Long exampleId) {
+	public String editExample(Model model, @PathVariable(name = "id", required = false) Long exampleId) {
 		Example example;
 		if (exampleId == null) {
 			example = new Example(0, "", 0.0);
@@ -25,13 +28,13 @@ public class ExampleController {
 	}
 
 	@PostMapping(path = { "/example/edit", "/example/edit/{id}" })
-	public String editProduct(Model model, Example example, BindingResult result) {
-		
+	public String editExample(Model model, Example example, BindingResult result) {
+
 		if (result.hasErrors()) {
 			result.addError(new ObjectError("example", "Biçimde yanlışlık var"));
 			return "/crud/ExampleEdit";
 		}
-		
+
 		System.out.println("Saklanıyor " + example.getExampleId() + " " + example.getExampleName() + " "
 				+ example.getExampleValue());
 		model.addAttribute("message", "Başarıyla Saklandı");
@@ -39,4 +42,28 @@ public class ExampleController {
 		return "/crud/ExampleEdit";
 
 	}
+
+	@GetMapping(value = "/example/list")
+	public String listExample(Model model) {
+		List<Example> exampleList = new ArrayList<>();
+		exampleList.add(new Example(701, "Örnek 1", 700));
+		exampleList.add(new Example(702, "Örnek 2", 800));
+		exampleList.add(new Example(703, "Örnek 3", 900));
+		model.addAttribute("exampleList", exampleList);
+		model.addAttribute("message", "Örnek dizelgesi");
+		return "/crud/ExampleList";
+	}
+
+	@GetMapping(value = "/example/delete/{id}")
+	public String removeExample(@PathVariable("id") long exampleId, Model model) {
+		System.out.println("Siliniyor.. " + exampleId);
+		List<Example> exampleList = new ArrayList<>();
+		exampleList.add(new Example(701, "Örnek 1", 700));
+		exampleList.add(new Example(702, "Örnek 2", 800));
+		exampleList.add(new Example(703, "Örnek 3", 900));
+		model.addAttribute("exampleList", exampleList);
+		model.addAttribute("message", "Silindi");
+		return "/crud/ExampleList";
+	}
+
 }
